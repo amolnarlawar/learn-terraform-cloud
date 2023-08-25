155efaf3d333ca9b530d3a8ec0547d1b60bf57e4
amol-gitlab <amol@yourdomain.com>
2023-08-26 00:47:29 +0530

    fff

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+  key_name = "key_pair_name"
+  tags = {
+    Name = "Demo"
+  }
+}
